d923b685c78a94a951d1dbde79686714e14d7bbd
Rahul saini <rs7965498@gmail.com>
2023-03-02 09:03:57 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import StateWise from './stateWise/StateWise';
+const App = () => {
+  return (
+    <div>
+            <StateWise />
+    </div>
+  )
+}
+
+export default App
